60589189948368ba9a2f4efb9833aca733ead07b
joseph finlayson <joseph.finlayson@gmail.com>
2013-10-06 17:33:36 +0100

    site framework

---
+++ application.js
@@ -0,0 +1,25 @@
+$(document).ready( function(){
+	$('#formsubmit').click(function(e){
+		console.log("asdf");
+		e.preventDefault();
+		var email = $('#email').val();
+		var name = $('#name').val();
+		console.log(email+name);
+		if ((email != 'email address') && name != 'name') { // && (name != '' || 'name') ) 
+			$.ajax({
+				  dataType: 'jsonp',
+				  url: "http://getsimpleform.com/messages/ajax?form_api_token=11ea25855a13b79371ce915c054cda86",
+				  data: {
+				    name: name,
+				    email: email,
+				  }
+				}).done(function() {
+				  //callback which can be used to show a thank you message
+				  //and reset the form
+				  alert("Thank you for leaving your details, you will be added to the mailing list shortly");
+				  email.val('');
+				  name.val('');
+				});
+		} else { alert("Your details are incorrect")}
+	});
+});
